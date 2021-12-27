93dccf6dc30694eef1e75f42663bd68e0981b998
Melissa Yung <yung.melissa@gmail.com>
2021-12-27 19:51:24 +1100

    Adds purge method

--- Sources/RemoteImageView/RemoteImageFetcher.swift
+++ Sources/RemoteImageView/RemoteImageFetcher.swift
@@ -30,30 +30,34 @@ import Foundation
 import SwiftUI
 
 public class RemoteImageFetcher: ObservableObject {
-  @Published var imageData = Data()
-  let url: URL
+    @Published var imageData = Data()
+    let url: URL
 
-  public init(url: URL) {
-    self.url = url
-  }
+    public init(url: URL) {
+        self.url = url
+    }
 
-  // 1
-  public func fetch() {
-    URLSession.shared.dataTask(with: url) { (data, _, _) in
-      guard let data = data else { return }
-      DispatchQueue.main.async {
-        self.imageData = data
-      }
-    }.resume()
-  }
+    // 1
+    public func fetch() {
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.imageData = data
+            }
+        }.resume()
+    }
 
-  // 2
-  public func getImageData() -> Data {
-    return imageData
-  }
+    // 2
+    public func getImageData() -> Data {
+        return imageData
+    }
 
-  // 3
-  public func getUrl() -> URL {
-    return url
-  }
+    // 3
+    public func getUrl() -> URL {
+        return url
+    }
+
+    public func purge() {
+        imageData = Data()
+    }
 }
